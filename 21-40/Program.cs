2ac32911e0241a6aafd68545f6a688c53cf930d9
Davit <ghrzoyandavit@gmail.com>
2022-11-28 18:17:12 +0400

    Add project files.

---
+++ 21-40/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+
+namespace _21_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Xndir 21 
+            ///
+            //int a = 1;   
+            //int b = 2;
+            //int c = 3;
+
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine("a is the biggest");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    Console.WriteLine("b is the biggest");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("c is the biggest");
+
+
+            //}
+
+            //Xndir 22
+
+            //int a = 15;
+            //int b = 20;
+            //int c = 8;
+
+            //if (a < b && a < c)
+            //{
+            //    Console.WriteLine("a is the smallest");
+            //}
+            //else if (b < a && b < c)
+            //{
+            //    Console.WriteLine("b is the smallest");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("c is the smallest");
+
+            ////Xndir 23
+            //int a = 2;
+            //int b = 1;
+            //int c = 10;
+
+            //if (a == 1 || b == 1 || c == 1) 
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //    Console.WriteLine("False");
+
+            //// Xndir 24
+            //int a = 2;
+            //int b = 2;
+            //int c = 9;
+
+            //if (a == 2 && b == 2)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else if (a == 2 && c == 2)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else if (b == 2 && c == 2)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //    Console.WriteLine("False");
+
+            ////Xndir 25
+
+            //int a = 0;
+            //int b = 12;
+            //int c = 10;
+
+            //if (a > 1 && b > 1 && c > 1)
+            //{
+            //    Console.WriteLine(" y=1");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" y=2 ");
+            //}
+
+            ////Xndir 26
+            //int a = 97;
+            //int b = 100;
+            //int c = 3;
+
+            //if(a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            //{
+            //    Console.WriteLine(" 1 ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" 2 ");
+            //}
+
+            ////Xndir 27
+
+            //int a = 2;
+            //int b = 5;
+            //int c = 8;
+
+            //if (c-b == b-a)
+            //{
+            //    Console.WriteLine(" True, it's an Arithmetic progression");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine(" False, it's not Arithmetic progression");
+            //}
+
+            ////Xndir 28
+
+            //int a = 2;
+            //int b = 4;
+            //int c = 8;
+
+            //if (c/b == b/a)
+            //{
+            //    Console.WriteLine("True, it's a geometric progression");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False, it's not a geometric progression");
+            //}
+
+            ////Xndir 29
+
+            //int a = 100;
+            //int b = 9;
+            //int c = 99;
+
+            //if (a > b && a > c && b > c)
+            //{
+            //    Console.WriteLine("c, b, a");
+            //}
+            //else if (b > a && b > c && a > c)
+            //{
+            //    Console.WriteLine("c, a, b");
+            //}
+            //else if (c > a && c > b && a > b)
+            //{
+            //    Console.WriteLine("b, a, c");
+            //}
+            //else if (a > b && a > b && c > b)
+            //{
+            //    Console.WriteLine("b, c, a");
+            //}
+            //else
+            //    Console.WriteLine("3 non-consecutive numbers");
+
+            ////Xndir 30
+
+            //int a = 3;
+            //int b = 5;
+            //int c = 2;
+
+            //if (a > b && a > c && b > c)
+            //{
+            //    Console.Write(a);
+            //    Console.Write(b);
+            //    Console.Write(c);
+            //}
+            //if (a > b && a > c && c > b)
+            //{
+            //    Console.Write(a);
+            //    Console.Write(c);
+            //    Console.Write(b);
+            //}
+            //if (b > a && b > c && a > c)
+            //{
+            //    Console.Write(b);
+            //    Console.Write(a);
+            //    Console.Write(c);
+            //}
+            //if (b > a && b > c && c > a)
+            //{
+            //    Console.Write(b);
+            //    Console.Write(c);
+            //    Console.Write(a);
+            //}
+            //if(c > a && c > b && a >b)
+            //{
+            //    Console.Write(c);
+            //    Console.Write(a);
+            //    Console.Write(b);
+            //}
+            //if (c > a && c > b && b > a)
+            //{
+            //    Console.Write(c);
+            //    Console.Write(b);
+            //    Console.Write(a);
+            //}
+
+            ////Xndir 31
+
+            //int a = 20;
+            //int b = 100;
+            //int c = 30;
+            //int d = 400;
+
+            //if (a > b && a > c && a > d)
+            //{
+            //    Console.WriteLine("a is the biggest");
+            //}
+            //else if (b > a && b > c && b > d)
+            //{
+            //    Console.WriteLine("b is the biggest");
+            //}
+            //else if (c > a && c > b && c > d)
+            //{
+            //    Console.WriteLine("c is the biggest");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("d is the biggest");
+            //}
+
+            ////Xndir 32
+
+            //int a = 2000;
+            //int b = 100;
+            //int c = 30;
+            //int d = 400;
+
+            //if (a < b && a < c && a < d)
+            //{
+            //    Console.WriteLine("a is the smallest");
+            //}
+            //else if (b < a && b < c && b < d)
+            //{
+            //    Console.WriteLine("b is the smallest");
+            //}
+            //else if (c < a && c < b && c < d)
+            //{
+            //    Console.WriteLine("c is the smallest");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("d is the smallest");
+            //}
+
+            ////Xndir 33
+
+            //int a = 1;
+            //int b = 8;
+            //int c = 10;
+            //int d = 8;
+
+            //if (a == 1 || b == 1 || c == 1 || d == 1)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //    Console.WriteLine("False");
+
+            ////Xndir 34
+
+            //int a = 2;
+            //int b = 4;
+            //int c = 7;
+            //int d = 9;
+
+            //if(a+b == c+d || a+c == b+d || b+c == a+d)
+            //{
+            //    Console.WriteLine("True");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //Xndir 35
+
+            int a = 2;
+            int b = 4;
+            int c = 7;
+            int d = 9;
+
+            if (a == (b+c+d)|| b == (a+c+c) || c == (a+b+d) || d == (a+b+c))
+            {
+                Console.WriteLine("True")
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
